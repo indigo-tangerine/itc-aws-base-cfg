64906dd6ed31c826bde8d84cf378e9d566144a48
rupert <rupert@indigo-tangerine.nl>
2022-04-26 22:11:53 +0200

    feat: add cloudtrail

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ITC - AWS Account Base Configuration
 
-[![.github/workflows/ci.yml](https://github.com/indigo-tangerine/itc-aws-base-cfg/actions/workflows/ci.yml/badge.svg)](https://github.com/indigo-tangerine/itc-aws-base-cfg/actions/workflows/ci.yml)
+[![CI workflow](https://github.com/indigo-tangerine/itc-aws-base-cfg/actions/workflows/ci-pr.yml/badge.svg)]
+[![CD workflow](https://github.com/indigo-tangerine/itc-aws-base-cfg/actions/workflows/cd-pr.yml/badge.svg)]
 
 ## Resources
 
---
+++ terraform/_s3_tfm_state.tf
@@ -0,0 +1,44 @@
+# TFM State bucket
+
+#tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket" "tfm_state" {
+
+  bucket = "tfm-state-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name = "tfm-state-${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_s3_bucket_acl" "tfm_state" {
+  bucket = aws_s3_bucket.tfm_state.id
+  acl    = "private"
+}
+
+
+resource "aws_s3_bucket_versioning" "tfm_state" {
+  bucket = aws_s3_bucket.tfm_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+#tfsec:ignore:aws-s3-ignore-public-acls
+resource "aws_s3_bucket_public_access_block" "tfm_state" {
+  bucket = aws_s3_bucket.tfm_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+#tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfm_state" {
+  bucket = aws_s3_bucket.tfm_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
