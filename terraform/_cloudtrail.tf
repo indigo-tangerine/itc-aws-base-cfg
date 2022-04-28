816bbb7c26b3dedc9d0462f457bba43a29c22920
rupert <rupert@indigo-tangerine.nl>
2022-04-28 21:25:05 +0200

    feat: compliance
    fix cloudtrail log arn

--- terraform/_cloudtrail.tf
+++ terraform/_cloudtrail.tf
@@ -3,7 +3,7 @@ resource "aws_cloudtrail" "main" {
   name                          = "${var.client}-default-trail"
   s3_bucket_name                = aws_s3_bucket.cloudtrail.id
   include_global_service_events = false
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_logging.arn
   is_multi_region_trail         = false
   is_organization_trail         = true
