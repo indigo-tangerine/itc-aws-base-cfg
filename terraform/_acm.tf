2ddc45b17e50b50ef440f6a99b859f608b969ab8
rupert <rupert@indigo-tangerine.nl>
2022-06-01 21:58:32 +0200

    feat: add wildcard cert

---
+++ terraform/_acm.tf
@@ -0,0 +1,26 @@
+resource "aws_acm_certificate" "wildcard" {
+  domain_name       = "*.${aws_route53_zone.dev_itc.name}"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "wildcard_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.dev_itc.zone_id
+}
+
+resource "aws_acm_certificate_validation" "wildcard" {
+  certificate_arn         = aws_acm_certificate.wildcard.arn
+  validation_record_fqdns = [for record in aws_route53_record.wildcard_validation : record.fqdn]
+}
