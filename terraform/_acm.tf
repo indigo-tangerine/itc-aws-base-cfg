1110c49640de9964232a9d43dd4d44b3af6f0060
rupert <rupert@indigo-tangerine.nl>
2022-06-16 20:18:47 +0200

    feat: DA3 add cognito
    add dns vars
    
    
    fix file mode
    
    
    fixed cognito mod, update config

--- terraform/_acm.tf
+++ terraform/_acm.tf
@@ -1,3 +1,4 @@
+# Default wildcard cert
 resource "aws_acm_certificate" "wildcard" {
   domain_name       = "*.${aws_route53_zone.dev_itc.name}"
   validation_method = "DNS"
@@ -23,4 +24,33 @@ resource "aws_route53_record" "wildcard_validation" {
 resource "aws_acm_certificate_validation" "wildcard" {
   certificate_arn         = aws_acm_certificate.wildcard.arn
   validation_record_fqdns = [for record in aws_route53_record.wildcard_validation : record.fqdn]
+}
+
+# Cognito auth certificate
+
+resource "aws_acm_certificate" "auth" {
+  domain_name       = "*.${aws_route53_zone.dev_itc.name}"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "auth_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.auth.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.dev_itc.zone_id
+}
+
+resource "aws_acm_certificate_validation" "auth" {
+  certificate_arn         = aws_acm_certificate.auth.arn
+  validation_record_fqdns = [for record in aws_route53_record.auth_validation : record.fqdn]
 }
--- terraform/_route53.tf
+++ terraform/_route53.tf
@@ -1,3 +1,3 @@
 resource "aws_route53_zone" "dev_itc" {
-  name = "dev.indigo-tangerine.nl"
+  name = local.dev_dns_name
 }
