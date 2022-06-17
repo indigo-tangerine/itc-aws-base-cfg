4f14c58d6afe5fe54afe458942fdccab3963501e
rupert <rupert@indigo-tangerine.nl>
2022-06-17 22:27:46 +0200

    feat: DA-3 add cognito secrets
    add endpoint to secret
    
    
    add tfsec ignore

--- terraform/_cognito_clients.tf
+++ terraform/_cognito_clients.tf
@@ -14,16 +14,35 @@ resource "aws_cognito_resource_server" "mvdb" {
   user_pool_id = aws_cognito_user_pool.main.id
 }
 
-
 resource "aws_cognito_user_pool_client" "mvdb" {
   name = "mvdb-api"
 
   generate_secret                      = true
   allowed_oauth_flows                  = ["client_credentials"]
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_scopes                 = aws_cognito_resource_server.mvdb.scope_identifiers #["m2m"] #["m2m", "monitoring"]
+  allowed_oauth_scopes                 = aws_cognito_resource_server.mvdb.scope_identifiers
   supported_identity_providers         = ["COGNITO"]
   prevent_user_existence_errors        = "ENABLED"
 
   user_pool_id = aws_cognito_user_pool.main.id
+}
+
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "mvdb" {
+  name = "${var.client}-mvdb"
+}
+
+resource "aws_secretsmanager_secret_version" "mvdb" {
+  secret_id     = aws_secretsmanager_secret.mvdb.id
+  secret_string = jsonencode(local.mvdb_cognito)
+}
+
+locals {
+  mvdb_cognito = {
+    client_id        = aws_cognito_user_pool_client.mvdb.id
+    client_secret    = aws_cognito_user_pool_client.mvdb.client_secret
+    m2m_scope        = "m2m"
+    monitoring_scope = "monitoring"
+    endpoint         = aws_cognito_user_pool.main.endpoint
+  }
 }
